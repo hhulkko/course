3d43b9cbe47729dae0b65099cdd775ad5a04bb42
hhulkko <heikki.hulkko@iki.fi>
2014-09-23 08:49:00 +0300

    Apply

--- src/Course/Apply.hs
+++ src/Course/Apply.hs
@@ -29,8 +29,7 @@ instance Apply Id where
     Id (a -> b)
     -> Id a
     -> Id b
-  (<*>) =
-    error "todo"
+  (<*>) (Id f) (Id a) = Id (f a)
 
 -- | Implement @Apply@ instance for @List@.
 --
@@ -41,8 +40,7 @@ instance Apply List where
     List (a -> b)
     -> List a
     -> List b
-  (<*>) =
-    error "todo"
+  (<*>) fs xs = flatMap (\f -> map f xs) fs
 
 -- | Implement @Apply@ instance for @Optional@.
 --
@@ -59,8 +57,7 @@ instance Apply Optional where
     Optional (a -> b)
     -> Optional a
     -> Optional b
-  (<*>) =
-    error "todo"
+  (<*>) f x = bindOptional (\g -> mapOptional g x) f
 
 -- | Implement @Apply@ instance for reader.
 --
@@ -83,8 +80,7 @@ instance Apply ((->) t) where
     ((->) t (a -> b))
     -> ((->) t a)
     -> ((->) t b)
-  (<*>) =
-    error "todo"
+  (<*>) f g = (\x -> f x (g x))
 
 -- | Apply a binary function in the environment.
 --
@@ -111,8 +107,7 @@ lift2 ::
   -> f a
   -> f b
   -> f c
-lift2 =
-  error "todo"
+lift2 f fa fb = f <$> fa <*> fb
 
 -- | Apply a ternary function in the environment.
 --
@@ -143,8 +138,7 @@ lift3 ::
   -> f b
   -> f c
   -> f d
-lift3 =
-  error "todo"
+lift3 f fa fb fc = f <$> fa <*> fb <*> fc
 
 -- | Apply a quaternary function in the environment.
 --
@@ -176,8 +170,7 @@ lift4 ::
   -> f c
   -> f d
   -> f e
-lift4 =
-  error "todo"
+lift4 f fa fb fc fd = f <$> fa <*> fb <*> fc <*> fd
 
 -- | Sequence, discarding the value of the first argument.
 -- Pronounced, right apply.
@@ -202,8 +195,7 @@ lift4 =
   f a
   -> f b
   -> f b
-(*>) =
-  error "todo"
+(*>) fa fb = (\_ b -> b) <$> fa <*> fb
 
 -- | Sequence, discarding the value of the second argument.
 -- Pronounced, left apply.
@@ -228,8 +220,7 @@ lift4 =
   f b
   -> f a
   -> f b
-(<*) =
-  error "todo"
+(<*) fa fb = (\a _ -> a) <$> fa <*> fb
 
 -----------------------
 -- SUPPORT LIBRARIES --
